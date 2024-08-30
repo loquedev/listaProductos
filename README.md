b7d1531be13af40952799c4c724db1ce19909cab
Carlos Sáenz <dev.loque@gmail.com>
2024-08-30 16:52:30 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Nextjs & Mongodb CRUD
+
+A web aplication CRUD using Nodejs y Mongodb (with mongoose)
+
+### Installation
+
+```
+cd next-mongodb-app
+npm i
+npm run dev
+```
+# listadeprecio1
---
+++ src/utils/mongoose.js
@@ -0,0 +1,23 @@
+import { connect, connection } from "mongoose";
+
+const conn = {
+  isConnected: false,
+};
+
+export async function dbConnect() {
+  if (conn.isConnected) {
+    return;
+  }
+
+  const db = await connect(
+    "mongodb+srv://devloque:3k5VcuI97hUomtzx@cluster0.irxpzet.mongodb.net/nextjs?retryWrites=true&w=majority&appName=Cluster0"
+  );
+  // console.log(db.connection.db.databaseName);
+  conn.isConnected = db.connections[0].readyState;
+}
+
+
+
+connection.on("connected", () => console.log("Mongodb connected to db"));
+
+connection.on("error", (err) => console.error("Mongodb Errro:", err.message));
